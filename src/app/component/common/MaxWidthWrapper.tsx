2718248f479c23a1ebbdefa20ee032152520fb20
Avishek Dahal <dahalavishek2022@gmail.com>
2024-09-30 19:38:35 +0545

    fix: max width

--- src/app/component/common/MaxWidthWrapper.tsx
+++ src/app/component/common/MaxWidthWrapper.tsx
@@ -1,8 +1,20 @@
+import { cn } from '@/lib/utils';
 import { ReactNode } from 'react';
 
-const MaxWidthWrapper = ({ children }: { children: ReactNode }) => {
+const MaxWidthWrapper = ({
+  children,
+  width = 'min',
+}: {
+  children: ReactNode;
+  width?: string;
+}) => {
   return (
-    <div className="bg-blackPrimary h mx-auto w-full max-w-5xl px-2.5 lg:px-20">
+    <div
+      className={cn(
+        'h mx-auto w-full bg-blackPrimary px-2.5 lg:px-20',
+        width === 'max' ? 'max-w-7xl' : 'max-w-5xl'
+      )}
+    >
       {children}
     </div>
   );
